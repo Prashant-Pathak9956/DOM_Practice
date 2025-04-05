20bb263d24ceb176c9da71b230577fcb973eb43c
Prashant-Pathak9956 <90prashantpathak@gmail.com>
2025-04-05 20:16:55 +0530

    second commit

--- Day_9/script.js
+++ Day_9/script.js
@@ -1,15 +1,22 @@
 var btn = document.querySelector('button')
-var body = document.querySelector('body')
+var body = document.body
 
 btn.addEventListener('click', function(){
     var img = document.createElement('img')
-    img.setAttribute('src', 'https://e7.pngegg.com/pngimages/407/726/png-clipart-cartoon-school-cartoon-child-child-photography-thumbnail.png')
+    
     var x = Math.random()*100
     var y = Math.random()*100
     var z = Math.random()*360
     img.style.left = x+'%'
-    img.style.right = y+'%'
+    img.style.top = y+'%'
     img.style.rotate = z+'deg'
 
+    img.setAttribute('src','https://www.freeiconspng.com/uploads/spongebob-cartoon-png-32.png')
     body.appendChild(img)
+
 })
+
+
+   
+    
+   
